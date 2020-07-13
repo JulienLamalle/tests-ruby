951bfa6dfe0e234cf73c8332c9fa7d982fbb4421
Julien Lamalle <eparis@atlassian.com>
2020-07-13 19:04:49 +0200

    I commit my exercices 6

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,15 +1,24 @@
-def translate(s)
+
+def translate(string)
   alphabet = ('a'..'z').to_a
   vowels = ["a","e","i","o","u"] 
   consonants = alphabet - vowels
-
-  if vowels.include?(s[0])
-    s + 'ay'
-  elsif consonants.include?(s[0]) && consonants.include?(s[1])
-    s[2..-1] + s[0..1] + 'ay'
-  elsif consonants.include?(s[0])
-    s[1..-1] + s[0] + 'ay'
-  else
-    s
+  string  = string.split.map! do |w|
+    if vowels.include?(w[0])
+      w + 'ay'
+    elsif w [0..1] == "qu" 
+      w[2..-1] + "quay"
+    elsif w[0..2] == "thr"
+      w [3..-1]+"thray"
+    elsif w[0..2]== "sch"
+      w[3..-1]+"schay"
+    elsif (consonants.include?(w[0]) && consonants.include?(w[1]) && consonants.include?(w[2])) || w[1..2] == "qu"
+      w [3..-1] + w [0..2]+ 'ay'
+    elsif consonants.include?(w[0]) && consonants.include?(w[1])
+      w [2..-1] + w [0..1]+ 'ay'
+    else consonants.include?(w[0])
+      w[ 1..-1] + w[0..0] + 'ay' 
+    end
   end
+  return string.join(" ")
 end
